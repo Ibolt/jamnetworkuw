3ed9da44d942c6abd198f4f9d844455244a15417
Ibolt <imaangill@outlook.com>
2024-02-04 15:35:34 -0500

    Bump revalidation time for forms page to 1s

--- src/app/forms/page.tsx
+++ src/app/forms/page.tsx
@@ -1,7 +1,7 @@
 import { listForms } from "../api/googleApiController";
 import BaseFilePage from "../components/baseFilePage";
 
-export const revalidate = 0;
+export const revalidate = 1;
 export default async function FormsPage() {
   const files = await listForms();
 
