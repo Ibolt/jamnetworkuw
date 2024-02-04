810040c075a1931af6786d4ac1eb9af2c94b6993
Ibolt <imaangill@outlook.com>
2024-02-04 15:28:49 -0500

    Force dynamic rendering of forms page

--- src/app/forms/page.tsx
+++ src/app/forms/page.tsx
@@ -1,6 +1,7 @@
 import { listForms } from "../api/googleApiController";
 import BaseFilePage from "../components/baseFilePage";
 
+export const revalidate = 0;
 export default async function FormsPage() {
   const files = await listForms();
 
