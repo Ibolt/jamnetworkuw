19df91fd484eb0e31751673e0ae531c66f183f63
Ibolt <imaangill@outlook.com>
2024-02-18 15:51:20 -0500

    Fix jamboree event desc

--- src/app/components/homepage-sections/events.tsx
+++ src/app/components/homepage-sections/events.tsx
@@ -28,7 +28,7 @@ const Events = () => {
     "We bring our instruments (or our voices), split off into groups if needed and play music together. We converse, connect, figure out what songs we like, and then jam out to them!",
     "People show off their sick skills, an original song they wrote or a cover they’ve been working on recently.",
     "We pick an instrument/skill and learn it together. Sometimes, we have one or more people who are able to teach it and sometimes, we just rent out an instrument that nobody knows how to play and figure it out!",
-    "An end of term that's too special to be called an end of term concert, the Jamboree is the culmination of a term's worth of hard work and practice. It's a chance for everyone to show off what they've learned and how they've grown as musicians!",
+    "An end of term event that's too special to be called an EOT Concert, the Jamboree is the culmination of months of worth of hard work and practice. It's a chance for everyone to show off what they've learned and how they've grown as musicians to our biggest audience of the term!",
   ];
 
   return (
