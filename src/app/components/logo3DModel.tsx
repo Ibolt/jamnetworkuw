1f2bad31361d3aa956d104f35ff98b90f5324af8
Ibolt <imaangill@outlook.com>
2024-02-18 15:40:09 -0500

    Update spline animation

--- src/app/components/logo3DModel.tsx
+++ src/app/components/logo3DModel.tsx
@@ -5,7 +5,7 @@ import "../../styles/homepage-sections/logo3DModel.css";
 
 export default function Logo3DModel() {
   const SPLINE_LINK =
-    "https://prod.spline.design/3zqwixtM52sLQcIb/scene.splinecode";
+    "https://prod.spline.design/G7AW3OKpjV4oWnPn/scene.splinecode";
 
   // TODO: Logo clipped by container on mobile
   return <Spline scene={SPLINE_LINK} className="spline-container" />;
