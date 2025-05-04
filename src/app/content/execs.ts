bcfbe5c49266a7f009d94b4d7183449be71376f8
Ibolt <imaangill@outlook.com>
2025-05-04 14:24:48 -0400

    Exec updates

--- src/app/content/execs.ts
+++ src/app/content/execs.ts
@@ -1,32 +1,27 @@
 export const EXECS_DATA = [
   {
-    src: "/media/execs/adair.jpg",
-    name: "Adair Tran",
-    role: "Co-President | Media, Marketing, and Design Manager",
+    src: "/media/execs/melina.jpg",
+    name: "Melina Liu",
+    role: "President",
   },
   {
     src: "/media/execs/julia.jpg",
     name: "Julia You",
-    role: "Co-President | Media, Marketing, and Design Manager",
+    role: "Treasurer",
   },
   {
-    src: "/media/execs/imaan.jpg",
-    name: "Imaan Gill",
-    role: "Secretary | Treasurer",
-  },
-  {
-    src: "/media/execs/bastian.jpg",
-    name: "Bastian Perez",
+    src: "/media/execs/shobhit.jpg",
+    name: "Shobhit Chaudhary",
     role: "Equipment Team Director",
   },
   {
-    src: "/media/execs/celina.jpg",
-    name: "Celina Cayabyab",
+    src: "/media/execs/aerin.jpg",
+    name: "Aerin Schmall",
     role: "Assistant Events Leader",
   },
   {
-    src: "/media/execs/umer.jpg",
-    name: "Umer Riaz",
-    role: "Jamfreak",
+    src: "/media/execs/adair.jpg",
+    name: "Adair Tran",
+    role: "Project Manager",
   },
 ];
