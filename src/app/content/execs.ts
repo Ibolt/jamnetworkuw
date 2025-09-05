d347377d667317a4951adefe63bd6043524abb53
Ibolt <imaangill@outlook.com>
2025-09-05 19:47:56 -0400

    new execs

--- src/app/content/execs.ts
+++ src/app/content/execs.ts
@@ -1,27 +1,37 @@
 export const EXECS_DATA = [
   {
-    src: "/media/execs/melina.jpg",
-    name: "Melina Liu",
+    src: "/media/execs/shobhit.jpg",
+    name: "Shobhit Chaudhary",
     role: "President",
+  },
+  {
+    src: "/media/execs/cait.jpg",
+    name: "Cait Aitchison",
+    role: "Secretary",
+  },
+  {
+    src: "/media/execs/maia.jpg",
+    name: "Maia Bosma",
+    role: "Treasurer",
+  },
+  {
+    src: "/media/execs/harjosh.jpg",
+    name: "Harjosh Virdi",
+    role: "Media, Marketing, and Design Manager",
+  },
+  {
+    src: "/media/execs/phoebe.jpg",
+    name: "Phoebe Yang",
+    role: "Media, Marketing, and Design Manager",
+  },
+  {
+    src: "/media/execs/umer.jpg",
+    name: "Umer Riaz",
+    role: "Assistant Events Leader",
   },
   {
     src: "/media/execs/julia.jpg",
     name: "Julia You",
-    role: "Treasurer",
-  },
-  {
-    src: "/media/execs/shobhit.jpg",
-    name: "Shobhit Chaudhary",
-    role: "Equipment Team Director",
-  },
-  {
-    src: "/media/execs/aerin.jpg",
-    name: "Aerin Schmall",
     role: "Assistant Events Leader",
-  },
-  {
-    src: "/media/execs/adair.jpg",
-    name: "Adair Tran",
-    role: "Project Manager",
   },
 ];
