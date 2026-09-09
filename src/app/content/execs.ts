c4c3d8dc98a39a47f83584e759d384d0694b7764
Kareem Fahmi <60073461+Kareem168@users.noreply.github.com>
2026-09-08 21:41:57 -0400

    f26 execs

--- src/app/content/execs.ts
+++ src/app/content/execs.ts
@@ -1,42 +1,47 @@
 export const EXECS_DATA = [
   {
-    src: "/media/execs/s26/alex_bruh.png",
-    name: "Alex Rui He",
-    role: "President",
+    src: "/media/execs/f26/Shobhit.png",
+    name: "Shobhit",
+    role: "Co-President",
   },
   {
-    src: "/media/execs/s26/phoebe_guitar.png",
-    name: "Phoebe Yang",
-    role: "Media, Marketing, and Design Manager",
+    src: "/media/execs/f26/Lia.jpg",
+    name: "Lia",
+    role: "Co-President",
   },
   {
-    src: "/media/execs/s26/maggie.png",
-    name: "Maggie Qu",
-    role: "Media, Marketing, and Design Manager",
-  },
-  {
-    src: "/media/execs/s26/shobhit_outside.png",
-    name: "Shobhit Chaudhary",
-    role: "Equipment Team Director",
-  },
-  {
-    src: "/media/execs/s26/sahaj.png",
-    name: "Sahaj Nagra",
+    src: "/media/execs/f26/Ayanfe.png",
+    name: "Ayanfe",
     role: "Secretary",
   },
   {
-    src: "/media/execs/s26/lia.png",
-    name: "Lia Mendoca",
+    src: "/media/execs/f26/Arayansh.png",
+    name: "Arayansh",
     role: "Treasurer",
   },
   {
-    src: "/media/execs/s26/lexi.png",
-    name: "Lexi Marfil",
-    role: "Jam Recipe Writer",
+    src: "/media/execs/f26/Nicholas.png",
+    name: "Nicholas",
+    role: "Assistant Events Leader",
   },
   {
-    src: "/media/execs/s26/liam.png",
-    name: "Liam McCrory",
-    role: "Assistant Events Leader",
+    src: "/media/execs/f26/alex.png",
+    name: "Alex",
+    role: "Media, Marketing & Design Manager",
+  },
+  {
+    src: "/media/execs/f26/danyka.jpg",
+    name: "Danyka",
+    role: "Media, Marketing & Design Manager",
+  },
+  {
+    src: "/media/execs/f26/Will_blue_shirt.png",
+    name: "Will",
+    role: "Equipment Team Director",
+  },
+  {
+    src: "/media/execs/f26/Sahaj.png",
+    name: "Sahaj",
+    role: "Jam Journalist",
   },
 ];
